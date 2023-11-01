7c420a45b39bd963f7975d85d3aef9faa49ec361
Katimaus4711 <149404722+Katimaus4711@users.noreply.github.com>
2023-11-01 16:41:49 +0000

    Update streamlit_app.py
    changed response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 streamlit.title('My Parents New Healthy Diner')
